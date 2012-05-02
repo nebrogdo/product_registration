da7cf9d58dfb589fa14ad37c5e93100dc71a735e
Norm Brogdon, Jr <norm@thebytestuff.com>
2012-05-02 16:39:25 -0600

    Initial Commit of files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProductRegistration::Application.config.secret_token = '0be417ae21761121dd314b41d342ed0347ad0167b9354e9d2254f0808a1ed9718f6d5d6165ee32f1f12d3eae46fdc1df8879923eae14efc6b81b6cc432dc20c6'
